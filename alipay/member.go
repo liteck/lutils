0351096b07bf70ed9a36d32a7a8bd4743acec351
顾晨凯 <gchk125@163.com>
2017-07-17 16:37:44 +0800

    增加会员接口

---
+++ alipay/member.go
@@ -0,0 +1,36 @@
+/**
+ 会员api
+
+**/
+package alipay
+
+/**
+支付宝会员授权信息查询接口
+alipay.user.info.share
+配合支付宝会员授权接口，根据授权token，查询授权信息。
+*/
+type AlipayUserInfoShare struct {
+	AlipayApi
+}
+
+func (a *AlipayUserInfoShare) SetAppId(app_id string) error {
+	a.setApiMethod(a.apiMethod())
+	a.setApiName(a.apiName())
+	return a.AlipayApi.SetAppId(app_id)
+}
+
+func (k *AlipayUserInfoShare) apiMethod() string {
+	return "alipay.user.info.share"
+}
+
+func (k *AlipayUserInfoShare) apiName() string {
+	return "支付宝会员授权信息查询接口"
+}
+
+func (a *AlipayUserInfoShare) packageBizContent() string {
+	return ""
+}
+
+func init() {
+	registerApi(new(AlipayUserInfoShare))
+}
